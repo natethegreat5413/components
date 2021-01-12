dc9e0d7b4fd59bf36d5dd140e45a1083165af19e
Nathan Cowley <nathancowley12@gmail.com>
2021-01-11 19:02:13 -0700

    props working and the card looks good

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = ({ author }) => {
+const CommentDetail = ({ author, timeAgo, comments, profilePic }) => {
 	return (
 		<div className="comment">
 			<a href="/" className="avatar">
-				<img alt="avatar" src={faker.image.image()} />
+				<img alt="avatar" src={profilePic} />
 			</a>
 			<div className="content">
 				<a href="/" className="author">
 					{author}
 				</a>
 				<div className="metadata">
-					<span className="date">{`Today at ${new Date().toLocaleTimeString()}`}</span>
+					<span className="date">{timeAgo}</span>
 				</div>
-				<div className="text">Nice blog post!</div>
+				<div className="text">{comments}</div>
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail author="nate" />
-			<CommentDetail author="joe" />
-			<CommentDetail author="Fred" />
+			<ApprovalCard>
+				<CommentDetail
+					timeAgo="Today at 5:45PM"
+					author="nate"
+					comments="passing as props 1"
+					profilePic={faker.image.image()}
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					timeAgo="Today at 6:00AM"
+					author="joe"
+					comments="passing as props 2"
+					profilePic={faker.image.image()}
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					timeAgo="Today at 8:45PM"
+					author="Fred"
+					comments="passing as props 3"
+					profilePic={faker.image.image()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
